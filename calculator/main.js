8af3edacf9a13e8a222609f69d3efb6cf522fe47
bondparkerbond <parkerbond@gmail.com>
2015-12-14 16:50:48 -0700

    clean up calculator code

--- calculator/main.js
+++ calculator/main.js
@@ -4,38 +4,13 @@ function getInputs(){
   return [inputOne, inputTwo];
 }
 
-// function add(){
-//   var inputs = getInput();
-//   var output = inputs[0] + inputs[1];
-//   setOutput(output);
-// }
-
-// function subtract(){
-//   var inputOne = document.getElementById('input_1').value;
-//   var inputTwo = document.getElementById('input_2').value;
-//   var output = parseInt(inputOne) - parseInt(inputTwo);
-//   document.getElementById('output').innerHTML = output;
-// }
-
-// function multiply(){
-//   var choice = 3;
-//   return choice;
-// }
-
-// function divide(){
-//   var inputs = getInputs();
-//   var calculate = inputs[0] / inputs[1];
-//   setOutput(calculate);
-// }
 var math = 0
-
 function operator(choice){
   math = choice;
 }
 
 function calculate(){
   var inputs = getInputs();
-  // var math = operator();
   if (math == 1){
     var output = inputs[0] + inputs[1];
   }
@@ -53,7 +28,3 @@ function calculate(){
   }
   document.getElementById('output').innerHTML = output;
 }
-
-// function setOutput(){
-//   document.getElementById('output').innerHTML = output;
-// }
